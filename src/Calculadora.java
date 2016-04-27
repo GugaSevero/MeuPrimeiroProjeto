bb26907c4b7ea17994cb5d4bf53fbcbcbda730d5
Tecnico - TI <tecnico@epfserver.com.br>
2016-04-26 22:38:13 -0300

    Criação dos métodos somar, subtrair, dividir e multiplicar

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,24 @@
 
 public class Calculadora {
+	
+	private int valorUm;
+	private int valorDois;
+	
+	public void somar(){
+		
+	}
+	
+	public void subtrair(){
+		
+	}
+	
+	public void dividir(){
+		
+	}
+	
+	public void multiplicar(){
+		
+	}
 
 	public static void main(String[] args) {
 
